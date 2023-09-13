fca2483e92ed277b00caef3af0951159c71668c1
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-09-13 10:02:15 -0600

    Update characters.js

--- js/characters.js
+++ js/characters.js
@@ -1,1 +1,30 @@
+const names = ["James Bond","Vesper Lynd","Q","Ernst Stavro Blofeld","M","Miss Moneypenny","Le Chieffre","Felix Leiter","Paloma","Camille Montes"];
 
+const imgs = ["James Bond","Vesper Lynd","Q","Ernst Stavro Blofeld","M","Miss Moneypenny","Le Chieffre","Felix Leiter","Paloma","Camille Montes"];
+
+const links = ["james-bond","vesper-lynd","q","ernst-stavro-blofeld","m","miss-moneypenny","le-chieffre","felix-leiter","paloma","camille-montes"];
+
+let Container = document.getElementById("container");
+
+let count;
+
+count = 0;
+
+function Generate() {
+
+  let img = document.createElement("img");
+  let name = document.createElement("p");
+  let link = document.createElement("a");
+  
+  count++;
+
+  link.setAttribute("href", `/html/${links[count - 1]}.html`);
+  img.setAttribute("src", `/images/characters/$[count]}.png`);
+  name.textContent = "" + names[count - 1];
+  
+  container.appendChild(link);
+  link.appendChild(img);
+  link.appendChild(name);
+};
+
+imgs.forEach(Generate);
