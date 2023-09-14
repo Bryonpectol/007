8655e8c1b4c786f081e100cac9e63de0b5cc0453
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-09-13 18:19:20 -0600

    Update books.js

--- js/books.js
+++ js/books.js
@@ -2,7 +2,7 @@ const names = ["Casino Royale","Live and Let Die","Moonraker","Diamonds are Fore
 
 const imgs = ["Casino Royale","Live and Let Die","Moonraker","Diamonds are Forever","From Russia with Love","Dr. No","Goldfinger","For Your Eyes Only","Thunderball","The Spy Who Loved Me","On Her Majesty's Secret Service","You Only Live Twice","The Man with the Golden Gun","Octopussy and the Living Daylights"];
 
-const links = ["casino-royale","spectre","skyfall","quantum-of-solace","casino-royale"];
+const links = ["casino-royale","live-and-let-die","moonraker","diamonds-are-forever","from-russia-with-love","dr-no","goldfinger","for-your-eyes-only","thunderball","the-spy-eho-loved-me","on-her-majestys-secret-service","you-only-live-twice","the-man-with-the-golden-gun","octopussy-and-the-living-daylights"];
 
 let Container = document.getElementById("container");
 
