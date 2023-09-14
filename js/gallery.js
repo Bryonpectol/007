3f4b0b3bf81bdd5d5560d534edce8b8b42e87685
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-09-13 19:37:28 -0600

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
-const imgs = ["Forever and a Day","Casino Royale","Live and Let Die","Moonraker","Diamonds are Forever","From Russia with Love","Dr. No","Goldfinger","For Your Eyes Only","Thunderball","The Spy Who Loved Me","On Her Majesty's Secret Service","You Only Live Twice","The Man with the Golden Gun","Octopussy and the Living Daylights"];
+const imgs = ["Forever and a Day","Casino Royale","Live and Let Die","Moonraker","Diamonds are Forever","From Russia with Love","Dr. No","Goldfinger","For Your Eyes Only","Thunderball","The Spy Who Loved Me","On Her Majesty's Secret Service","You Only Live Twice","The Man with the Golden Gun","Octopussy and the Living Daylights","","","","","","","","","","","","","","","","","","","",""];
 
-const links = ["forever-and-a-day","casino-royale","live-and-let-die","moonraker","diamonds-are-forever","from-russia-with-love","dr-no","goldfinger","for-your-eyes-only","thunderball","the-spy-eho-loved-me","on-her-majestys-secret-service","you-only-live-twice","the-man-with-the-golden-gun","octopussy-and-the-living-daylights"];
+const links = ["forever-and-a-day","casino-royale","live-and-let-die","moonraker","diamonds-are-forever","from-russia-with-love","dr-no","goldfinger","for-your-eyes-only","thunderball","the-spy-eho-loved-me","on-her-majestys-secret-service","you-only-live-twice","the-man-with-the-golden-gun","octopussy-and-the-living-daylights","","","","","","","","","","","","","","","","","","","",""];
 
 let Container = document.getElementById("container");
 
@@ -16,8 +16,8 @@ function Generate() {
   
   count++;
 
-  link.setAttribute("href", `/images/gallery${links[count - 1]}.png`);
-  img.setAttribute("src", `/images/gallery/${[count - 1]}.png`);
+  link.setAttribute("href", `/images/gallery/${count}.png`);
+  img.setAttribute("src", `/images/gallery/${count}.png`);
 
     img.className = "artimgOne";
     link.className = "celebLinkClass";
