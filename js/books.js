a6f2770acf3341d36473a3d0b8a4f281cac3dc14
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-09-13 18:10:51 -0600

    Update books.js

--- js/books.js
+++ js/books.js
@@ -1,4 +1,4 @@
-const names = ["No Time to Die","Spectre","Skyfall","Quantum of Solace","Casino Royale"];
+const names = ["Casino Royale","Live and Let Die","Moonraker","Diamonds are Forever","From Russia with Love","Dr. No","Goldfinger","For Your Eyes Only","Thunderball","The Spy Who Loved Me","On Her Majesty's Secret Service","You Only Live Twice","The Man with the Golden Gun","Octopussy and the Living Daylights"];
 
 const imgs = ["Casino Royale","Live and Let Die","Moonraker","Diamonds are Forever","From Russia with Love","Dr. No","Goldfinger","For Your Eyes Only","Thunderball","The Spy Who Loved Me","On Her Majesty's Secret Service","You Only Live Twice","The Man with the Golden Gun","Octopussy and the Living Daylights"];
 
